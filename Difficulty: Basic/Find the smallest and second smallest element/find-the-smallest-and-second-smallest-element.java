3cdc6a188fc8dbeeeb48c69180da6f2ad44eb11d
Sandhya Ramkrushna Bramhane <141515283+Sandhya-Bramhane@users.noreply.github.com>
2024-07-31 13:35:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find the smallest and second smallest element/find-the-smallest-and-second-smallest-element.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[(int)(n)];
+            for (int i = 0; i < n; i++) {
+                arr[i] = (int)(Long.parseLong(inputLine[i]));
+            }
+
+            Solution obj = new Solution();
+            int[] product = obj.minAnd2ndMin(arr);
+            if (product[0] == -1)
+                System.out.println(product[0]);
+            else
+                System.out.println(product[0] + " " + product[1]);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int[] minAnd2ndMin(int arr[]) {
+        // code here
+        int min = Integer.MAX_VALUE;
+        int s_min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] < min)
+            {
+                s_min = min;
+                
+                min = arr[i];
+                
+            }
+            else if(arr[i] < s_min && arr[i] != min)
+            {
+                s_min = arr[i];
+               
+            }
+        }
+        
+        if(s_min == Integer.MAX_VALUE)
+        {
+            return new int[]{-1,-1};
+        }
+        
+        
+     return new int[]{min,s_min};
+    }
+}
+
+
+
+
+
+
+
+
+
+
